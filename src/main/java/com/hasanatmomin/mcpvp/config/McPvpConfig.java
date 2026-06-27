ed77c1e4246a7df2b4716e8e781329c0af5fd1c4
Maplized <260001826+HasanatMomin@users.noreply.github.com>
2026-06-27 11:38:27 +0600

    Delete src/main/java/com/hasanatmomin/mcpvp/config/McPvpConfig.java

--- src/main/java/com/hasanatmomin/mcpvp/config/McPvpConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hasanatmomin.mcpvp.config;
-
-public record McPvpConfig(
-        String defaultRegion,
-        int matchmakingIntervalTicks,
-        int maxTierGap,
-        int leaderboardSize
-) {
-}
